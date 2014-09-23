28d80d6a6854ba57e925e6797105333f1032a422
Tim Paine <t.paine154@gmail.com>
2014-09-23 12:20:45 -0400

    updates to cam testbench.

--- lab1/cam_test.sv
+++ lab1/cam_test.sv
@@ -1,6 +1,6 @@
 `include "param.sv"
 module cam_test #(parameter ARRAY_WIDTH_LOG2 = 5, parameter ARRAY_SIZE_LOG2 = 5)();
-`define clk_per 100
+`define clk_per 10
 
     logic clk;
     logic reset_i;
@@ -27,7 +27,67 @@ module cam_test #(parameter ARRAY_WIDTH_LOG2 = 5, parameter ARRAY_SIZE_LOG2 = 5)
     initial begin
 	/* $vcdpluson; */
 	// making logic signals 0 at start
-	
+    clk = 1'b0;
+    reset_1 = 1'b0;
+    read_i = 1'b0;
+    read_index_i = 'b0;
+    write_i = 1'b0;
+    write_index_i = 'b0;
+    write_data_i = 'b0;
+    search_i = 1'b0;
+    search_data_i = 'b0;
+    #10;//clk low
+    write_index_i = 5'h00001;
+    write_data_i = 32'h0000_0001;
+    write_i = 1'b1;
+    #5 //clk high
+    #5 //clk low
+    write_index_i = 5'b00011;
+    write_data_i = 32'h0000_0003;
+    #5 //clk high
+    #5 //clk low
+    write_index_i = 5'h00101;
+    write_data_i = 32'h0000_0005;
+    #5 //clk high
+    #5 //clk low
+    write_index_i = 5'h00111;
+    write_data_i = 32'h0000_0007;
+    #5//h
+    #5 //l
+    write_i=1'b0;
+    read_index_i = 5'b00011;
+    read_i = 1'b1;
+    #5 //h
+    read_index_i = 5'b00100;
+    read_i = 1'b1;
+    #5 //l
+    read_index_i = 5'b00101;
+    read_i = 1'b1;
+    search_i = 1'b1;
+    search_data_i = 32'h0000_0005;
+    write_index_i = 5'b01001;
+    write_data_i = 32'h0000_0009;
+    write_i = 1'b1;
+    #5 //h
+    read_index_i = 5'b00101;
+    read_i = 1'b1;
+    write_index_i = 5'b00101;
+    write_data_i = 32'h0000_0009;
+    write_i = 1'b1;
+    #5 //l
+    search_i = 1'b1;
+    search_data_i = 32'h0000_0005;
+    read_index_i = 5'b00101;
+    read_i = 1'b1;
+    #5 //h
+    #5 //l
+    search_i = 1'b1;
+    search_data_i = 32'h0000_0009;
+    #5 //h
+    #5 //l
+    
+
+
     /* #2 $display("value of valid signal is %d\n",valid); */
 
 
