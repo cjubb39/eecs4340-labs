5bfe33bb526b1988d6015f488eb2e855f3015915
Tim Paine <t.paine154@gmail.com>
2014-09-24 00:12:25 -0400

    search fixed.

--- lab1/cam.sv
+++ lab1/cam.sv
@@ -21,19 +21,28 @@ parameter ARRAY_SIZE_LOG2 = 5
   output logic [ARRAY_WIDTH_LOG2 - 1:0] search_index_o
 );
  
+
 logic [2**ARRAY_WIDTH_LOG2 - 1:0] out_value; /* if we are reading */
 logic [ARRAY_WIDTH_LOG2 - 1:0] out_index; /* if we are searching */
 
+
 logic found;
 logic written;
+
+
 
 logic [2**ARRAY_WIDTH_LOG2 - 1:0] cam_i [2**ARRAY_SIZE_LOG2 - 1:0];
 wire [2**ARRAY_WIDTH_LOG2 - 1:0] cam_o [2**ARRAY_SIZE_LOG2 - 1:0];
 logic [2**ARRAY_SIZE_LOG2 - 1:0] cam_v_i; 
 wire [2**ARRAY_SIZE_LOG2 - 1:0] cam_v_o; 
 
+
+
 /* index for cam search */
-logic [ARRAY_SIZE_LOG2-1:0] i;
+logic [2**ARRAY_SIZE_LOG2-1:0] priority_in;
+logic [ARRAY_SIZE_LOG2-1:0] result;
+wire priority_res;
+
 
 assign read_valid_o = read_i && written;
 assign search_valid_o = search_i && found;
@@ -51,36 +60,60 @@ always_comb begin
         if(read_i) begin
             written = cam_v_o[read_index_i];
             /* not optimal to drive to 0 */
-						case(cam_v_o[read_index_i])
-							'0: out_value = out_value;
-							'1: out_value = cam_o[read_index_i];
-						endcase
+		case(cam_v_o[read_index_i])
+			1'b0: out_value = out_value;
+			1'b1: out_value = cam_o[read_index_i];
+		endcase
         end
     end
 end
       
 
-/* search functionality */
 
+priorityencoder #(.SIZE(ARRAY_SIZE_LOG2)) pri_inst (.inp_i(priority_in), .out_o(result), .valid_o(priority_res));
+
+/* search functionality */
 always_comb begin
     if(reset_i) begin
         out_index = 'b0;
-        found = 1'b0;
     end
     else begin
-        found = 1'b0;
-        // this is ugly 
-				
-        for(i='b0;i<2**ARRAY_SIZE_LOG2;i=i+1'b1) begin
-						if(cam_v_o[i]==1'b1) begin
-                if(cam_o[i] == search_data_i) begin
-                    found = 1'b1;
-                    out_index = i;
-                    i = 2**ARRAY_SIZE_LOG2; // break
-								end
-            end
+        if(search_i) begin
+          priority_in = {(cam_o[0]==search_data_i)&cam_v_o[0], 
+                               (cam_o[1]==search_data_i)&cam_v_o[1],         
+                               (cam_o[2]==search_data_i)&cam_v_o[2],         
+                               (cam_o[3]==search_data_i)&cam_v_o[3],         
+                               (cam_o[4]==search_data_i)&cam_v_o[4],         
+                               (cam_o[5]==search_data_i)&cam_v_o[5],         
+                               (cam_o[6]==search_data_i)&cam_v_o[6],         
+                               (cam_o[7]==search_data_i)&cam_v_o[7],         
+                               (cam_o[8]==search_data_i)&cam_v_o[8],         
+                               (cam_o[9]==search_data_i)&cam_v_o[9],         
+                               (cam_o[10]==search_data_i)&cam_v_o[10],         
+                               (cam_o[11]==search_data_i)&cam_v_o[11],         
+                               (cam_o[12]==search_data_i)&cam_v_o[12],         
+                               (cam_o[13]==search_data_i)&cam_v_o[13],         
+                               (cam_o[14]==search_data_i)&cam_v_o[14],         
+                               (cam_o[15]==search_data_i)&cam_v_o[15],         
+                               (cam_o[16]==search_data_i)&cam_v_o[16],         
+                               (cam_o[17]==search_data_i)&cam_v_o[17],         
+                               (cam_o[18]==search_data_i)&cam_v_o[18],         
+                               (cam_o[19]==search_data_i)&cam_v_o[19],         
+                               (cam_o[20]==search_data_i)&cam_v_o[20],         
+                               (cam_o[21]==search_data_i)&cam_v_o[21],        
+                               (cam_o[22]==search_data_i)&cam_v_o[22],         
+                               (cam_o[23]==search_data_i)&cam_v_o[23],         
+                               (cam_o[24]==search_data_i)&cam_v_o[24],        
+                               (cam_o[25]==search_data_i)&cam_v_o[25],         
+                               (cam_o[26]==search_data_i)&cam_v_o[26],         
+                               (cam_o[27]==search_data_i)&cam_v_o[27],         
+                               (cam_o[28]==search_data_i)&cam_v_o[28],         
+                               (cam_o[29]==search_data_i)&cam_v_o[29],         
+                               (cam_o[30]==search_data_i)&cam_v_o[30],         
+                               (cam_o[31] == search_data_i)&cam_v_o[31]};   
+            found = priority_res;
+            search_index_o = result;
         end
-
     end
 end
 
@@ -100,7 +133,7 @@ endgenerate
 always_ff @(posedge clk) begin
     if(reset_i) begin
         // dont bother reseting whole cam
-        cam_v_i <= '0;
+        cam_v_i <= 'b0;
     end
     else begin
         if(write_i)
