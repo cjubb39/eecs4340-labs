d903badec4469c640db7c70db373e302ed069834
Timothy Paine <tkp2108@columbia.edu>
2014-10-16 12:52:00 -0400

    ff testbench reads config file now too.

--- lab2/ff_tb.sv
+++ lab2/ff_tb.sv
@@ -1,35 +1,32 @@
 `timescale 1ns/1ns
 
 class transaction;
-	rand bit a;
 	bit out;
 	bit next;
-
-	function void golden_result;
+	
+	function void golden_result(bit reset, bit n);
 		out = next;
-		next = a;
-	endfunction
-
-	function bit last;
-		return next;
+		next = (reset==1) ? 0:n;
 	endfunction
 
 	function bit check_result(bit x);
 		return(x == out);
 	endfunction
 
-    function bit check_reset(bit x);
-        out=0;
-        next = 0;
-        return (x==0);
-    endfunction
+        function bit check_reset(bit x);
+        	return ((x==out)&&(next==0));
+        endfunction
 
 endclass
 
+
+
+
 class testing_env;
     rand int unsigned rn;
+    rand bit a;
+    
     int reset_thresh;
-    int write_thresh;
     int iter;
 
 function void read_config(string filename);
@@ -47,7 +44,7 @@ function void read_config(string filename);
         end else if("READ_PROB" == param) begin
             //read_thresh = value;
         end else if("WRITE_PROB" == param) begin
-            write_thresh = value;
+            //write_thresh = value;
         end else if("SEARCH_PROB" == param) begin
             //search_thresh = value;
         end else if("RESET_PROB" == param) begin
@@ -60,49 +57,59 @@ function void read_config(string filename);
     end
 endfunction
 
-function get_write();
-    return ((rn%1000)<write_thresh);
-endfunction
 function get_reset();
     return ((rn%1000)<reset_thresh);
 endfunction
 
 endclass
 
+
+
+
 program ff_tb(ff_ifc.bench ds);
 	transaction t;
-    testing_env v; 
+    	testing_env v; 
+
+	bit reset;
 
 	initial begin
 		t = new();
-        v = new();
-        v.read_config("config.txt");
+        	v = new();
+        	v.read_config("config.txt");
+
+
+		repeat(10) begin
+			ds.cb.reset <= 1'b1;
+			@(ds.cb);
+		end
+			ds.cb.reset <= 1'b0;
+			@(ds.cb);
 
 		repeat(v.iter) begin
-			t.randomize();
-            v.randomize();
+            		v.randomize();
 
-            write = v.get_write();
-            reset = v.get_reset();
+            		reset = v.get_reset();
 
 			// drive inputs for next cycle
-		    if(reset) begin
+		    	if(reset) begin
 			    $display("%t : %s \n", $realtime, "Driving reset");
-                ds.cb.reset <= 1'b1;
-            end else if(write) begin
-			    $display("%t : %s : %d \n", $realtime, "Driving value ", t.a);
-                ds.cb.data_i <= t.a;
-            end
+                	    ds.cb.reset <= 1'b1;
+            		end else begin
+				ds.cb.reset <= 1'b0;	
+			    	$display("%t : %s : %d \n", $realtime, "Driving value ", v.a);
+                	    	ds.cb.data_i <= v.a;
+            		end
 
 			@(ds.cb);
-			t.golden_result();
+			t.golden_result(reset, v.a);
 
-            if(reset) begin
+            		if(reset) begin
 			    $display("%t : %s \n", $realtime,t.check_reset(ds.cb.data_o)?"Pass-reset":"Fail-reset");
-            end else if(write) begin
+//			    $display("%d : %d \n", ds.cb.data_o, t.out);
+            		end else begin
 			    $display("%t : %s \n", $realtime,t.check_result(ds.cb.data_o)?"Pass-write":"Fail-write");
-            end
-
+//			    $display("%d : %d : %d \n", ds.cb.data_o, t.out, t.next);
+            		end
 		end
 	end
 
